2a7609cea34f38b473661e32bf2adc9ed1e0ab6c
AriGonzAtech <77697360+AriGonzAtech@users.noreply.github.com>
2023-06-29 15:45:18 -0500

    Update VMTemplatePostScript.sh

--- VMTemplatePostScript.sh
+++ VMTemplatePostScript.sh
@@ -4,8 +4,8 @@ sudo echo
 
 echo Create SSH keys if needed
 if [ ! -f /etc/ssh/ssh_host_ecdsa_key.pub ]; then
-    echo SSH keys exist.  Moving on. 
-else
-    echo Creating new SSH keys
+	echo Creating new SSH keys
 	sudo dpkg-reconfigure openssh-server
+else
+	echo SSH keys exist.  Moving on.
 fi
