119f7a10b82aa90848672fc54238642585c5e1b7
AriGonzAtech <77697360+AriGonzAtech@users.noreply.github.com>
2023-06-30 16:24:42 -0500

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,1 +1,19 @@
+#! /bin/bash
+sudo echo
+clear
+
+declare -A aliasList=( 
+	[getauthorized_keys]='"rm -f ~/authorized_keys.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/authorized_keys.sh && bash ~/authorized_keys.sh"'
+	
+ )
+
+
+for key in "${!aliasList[@]}"; do 
+  if ! grep -q "$key" ~/.bashrc; then
+      echo Adding alias 
+	    echo "alias $key=${aliasList[$key]}" >> ~/.bashrc
+  else
+      echo alias $key is already there
+  fi
+done
 
