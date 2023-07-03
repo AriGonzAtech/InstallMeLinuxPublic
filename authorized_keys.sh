498ddbae7ace245b7c8122bd3e1e41e9e2d5b296
AriGonzAtech <77697360+AriGonzAtech@users.noreply.github.com>
2023-07-03 10:46:11 -0500

    Update authorized_keys.sh

--- authorized_keys.sh
+++ authorized_keys.sh
@@ -3,6 +3,7 @@
 sshKeyArray=(	
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJZ4T0CcMMpyQRYYdNEq8nBYNV6yeIALa3qb2+/APaX TestKey"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII4moz7csWEn9m6RlHRxdRnZrvDg7AQ9HrKoClMf7uUP HasPW"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG7r/nR/RsmHE6cq/oMe/PttEeJ9TZR7gTNam/DnCXig AnsiblePubKey"
             )
 
 
