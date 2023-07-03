6c80d07ff84aabdc84dbd112eef21e05bd810c8b
AriGonzAtech <77697360+AriGonzAtech@users.noreply.github.com>
2023-07-03 11:13:09 -0500

    Update InstallAliasScript.sh

--- InstallAliasScript.sh
+++ InstallAliasScript.sh
@@ -2,11 +2,14 @@
 sudo echo
 clear
 
+gitRepo=https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme
+
+
 declare -A aliasList=( 
 	[getAuthorized_keysScript]='"rm -f ~/authorized_keys.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/authorized_keys.sh && sudo bash ~/authorized_keys.sh"'
 	[getAliasScript]='"rm -f ~/InstallAliasScript.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/InstallAliasScript.sh && sudo bash ~/InstallAliasScript.sh"'
  	[getInstallCTTemplatePostScript]='"rm -f ~/InstallCTTemplatePostScript.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/InstallCTTemplatePostScript.sh && sudo bash ~/InstallCTTemplatePostScript.sh"'
-  	[getInstallVMTemplatePreScript]='"rm -f ~/getInstallVMTemplatePreScript.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/getInstallVMTemplatePreScript.sh && sudo bash ~/getInstallVMTemplatePreScript.sh"'
+  	[getInstallVMTemplatePreScript]='"rm -f ~/InstallVMTemplatePreScript.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/InstallVMTemplatePreScript.sh && sudo bash ~/InstallVMTemplatePreScript.sh"'
  )
 
 
