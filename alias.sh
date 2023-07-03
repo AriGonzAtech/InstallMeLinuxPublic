6e1716c1c909606dda08c6812b7a335f60970884
AriGonzAtech <77697360+AriGonzAtech@users.noreply.github.com>
2023-07-03 10:39:29 -0500

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -3,8 +3,8 @@ sudo echo
 clear
 
 declare -A aliasList=( 
-	[getauthorized_keys]='"rm -f ~/authorized_keys.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/authorized_keys.sh && bash ~/authorized_keys.sh"'
-	[getAliasScript]='"rm -f ~/alias.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/alias.sh && bash ~/alias.sh"'
+	[getauthorized_keys]='"rm -f ~/authorized_keys.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/authorized_keys.sh && sudo bash ~/authorized_keys.sh"'
+	[getAliasScript]='"rm -f ~/alias.sh && wget https://raw.githubusercontent.com/AriGonzAtech/InstallMeLinuxPublic/installme/alias.sh && sudo bash ~/alias.sh"'
  )
 
 
